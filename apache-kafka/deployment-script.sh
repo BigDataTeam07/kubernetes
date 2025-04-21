60b9cd62d45e4b50258104490abfa1912aaccc23
chenyu <63953834+chenyu-01@users.noreply.github.com>
2025-04-21 12:21:54 +0800

    add spooldir

--- apache-kafka/deployment-script.sh
+++ apache-kafka/deployment-script.sh
@@ -6,3 +6,16 @@ kubectl apply -f kafka-config.yaml
 kubectl apply -f kafka-pvc.yaml
 kubectl apply -f kafka-statefulset.yaml
 kubectl apply -f kafka-service.yaml
+
+echo "Kafka deployment completed."
+# # to create topics, use the following commands to create topics
+# kubectl exec -it amazon-music-review-kafka-0 -n amazon-music-review -- /opt/kafka/bin/kafka-topics.sh --create --topic music-reviews-topic --partitions 3 --replication-factor 1 --bootstrap-server localhost:9092
+
+# kubectl exec -it amazon-music-review-kafka-0 -n amazon-music-review -- /opt/kafka/bin/kafka-topics.sh --create --topic social-media-topic --partitions 3 --replication-factor 1 --bootstrap-server localhost:9092
+
+# kubectl exec -it amazon-music-review-kafka-0 -n amazon-music-review -- /opt/kafka/bin/kafka-topics.sh --create --topic user-input-topic --partitions 3 --replication-factor 1 --bootstrap-server localhost:9092
+
+# kubectl exec -it amazon-music-review-kafka-0 -n amazon-music-review -- /opt/kafka/bin/kafka-topics.sh --create --topic recommendations-topic --partitions 3 --replication-factor 1 --bootstrap-server localhost:9092
+
+# Verify the topics
+# kubectl exec -it amazon-music-review-kafka-0 -n amazon-music-review -- /opt/kafka/bin/kafka-topics.sh --list --bootstrap-server localhost:9092
