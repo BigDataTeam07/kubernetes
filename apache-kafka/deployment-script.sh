86f05d08b33ee9448ab01cda0eafa14c42bad712
chenyu <63953834+chenyu-01@users.noreply.github.com>
2025-04-21 11:43:56 +0800

    fix: wait kafka namespace create first

--- apache-kafka/deployment-script.sh
+++ apache-kafka/deployment-script.sh
@@ -1,4 +1,7 @@
-
+kubectl apply -f kafka-namespace.yaml
+# pause for 5 seconds to allow the namespace to be created
+echo "Waiting for 5 seconds to allow the namespace to be created..."
+sleep 5
 kubectl apply -f kafka-config.yaml
 kubectl apply -f kafka-pvc.yaml
 kubectl apply -f kafka-statefulset.yaml
